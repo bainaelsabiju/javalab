f356ad41f2b2e761b4ea58a11a3f15ed278a97e8
bainaelsabiju <145443856+bainaelsabiju@users.noreply.github.com>
2023-09-19 15:54:45 +0530

    Add files via upload

---
+++ sumof2numbers.java
@@ -0,0 +1,13 @@
+package java;
+
+public class sumof2numbers {
+
+	public static void main(String[] args) {
+		
+	
+		int number1 , number2;
+		int sum=number1+number2;
+		System.out.print("sum="+sum);
+	}
+
+}
